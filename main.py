3b618cc9e168a777aa78a3ef8fea8c58bd1f05d6
Szymon Bryniak <105818379+SzymonBryniak@users.noreply.github.com>
2024-08-23 16:51:24 +0100

    day 26 progress with Data Overlap exercise

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ result = [number for number in numbers if number % 2 == 0]
 print(result)
 
 
+with open('file1.txt') as data1:
+    data1_object = data1.readlines()
+    intdata1 = [int(data.strip()) for data in data1_object]
+    
+    
+with open('file2.txt') as data2:
+    data2_object = data2.readlines()
+    intdata2 = [int(data.strip()) for data in data2_object]
+
+result = [data for data in intdata1 if data in intdata2]
+
+
+print(result)
